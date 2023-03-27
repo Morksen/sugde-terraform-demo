8aac42983952866d86b00e015f75d19781dcd235
Morksen <marcello.pabst@my-workstation.com>
2023-03-27 15:30:55 +0200

    add azurerm and random to providers

---
+++ examples/azure/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+
+  required_version = ">=1.3"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.37"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.4"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = "azure-example.tfstate"
+  }
+}
