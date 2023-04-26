91cf4059c6b5969c6435e76c29f3cf7f83c6d7b7
Morksen <marcello.pabst@my-workstation.com>
2023-04-26 10:39:12 +0200

    add logging from webapp to storage account

--- examples/azure/main.tf
+++ examples/azure/main.tf
@@ -5,6 +5,16 @@ resource "random_pet" "base" {
   }
 }
 
+resource "time_rotating" "main" {
+  rotation_rfc3339 = null
+  rotation_years   = 2
+
+  triggers = {
+    end_date = null
+    years    = 2
+  }
+}
+
 resource "azurerm_resource_group" "main" {
   name     = "${local.base_name}-main-rg"
   location = var.location
@@ -25,7 +35,13 @@ resource "azurerm_storage_account" "main" {
 }
 
 resource "azurerm_storage_container" "walogs_container" {
-  name                  = "wa-logs"
+  name                  = "application-logs"
+  container_access_type = "private"
+  storage_account_name  = azurerm_storage_account.main.name
+}
+
+resource "azurerm_storage_container" "httplogs_container" {
+  name                  = "http-logs"
   container_access_type = "private"
   storage_account_name  = azurerm_storage_account.main.name
 }
@@ -58,6 +74,24 @@ resource "azurerm_linux_web_app" "demo" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_service_plan.demo.location
   service_plan_id     = azurerm_service_plan.demo.id
+
+  logs {
+    application_logs {
+      azure_blob_storage {
+        retention_in_days = 30
+        level             = ("${terraform.workspace}" == "default") ? "Warning" : "Information"
+        sas_url           = format("https://${azurerm_storage_account.main.name}.blob.core.windows.net/${azurerm_storage_container.walogs_container.name}%s", data.azurerm_storage_account_blob_container_sas.application_logs.sas)
+      }
+      file_system_level = ("${terraform.workspace}" == "default") ? "Warning" : "Information"
+    }
+
+    http_logs {
+      azure_blob_storage {
+        retention_in_days = 30
+        sas_url           = format("https://${azurerm_storage_account.main.name}.blob.core.windows.net/${azurerm_storage_container.httplogs_container.name}%s", data.azurerm_storage_account_blob_container_sas.http_logs.sas)
+      }
+    }
+  }
 
   site_config {
     application_stack {
--- examples/azure/outputs.tf
+++ examples/azure/outputs.tf
@@ -12,3 +12,18 @@ output "log_analytics_workspace" {
   description = "The name of the created main log analytics workspace"
   value       = azurerm_log_analytics_workspace.main.name
 }
+
+output "web_app_url" {
+  description = "The URL of the created web app"
+  value       = azurerm_linux_web_app.demo.default_hostname
+}
+
+output "app_service_plan" {
+  description = "The name of the created service plan"
+  value       = azurerm_service_plan.demo.name
+}
+
+output "app_service_plan_sku" {
+  description = "The name of the created service plan"
+  value       = azurerm_service_plan.demo.sku_name
+}
